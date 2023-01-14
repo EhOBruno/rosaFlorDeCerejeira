3a53b1bf94a703b2747c68d227a9818007ca90a0
Felipe de Oliveira Motta <felipemotta949@gmail.com>
2023-01-14 18:48:15 -0300

    pagina-jogo

---
+++ front/js/teste.js
@@ -0,0 +1,25 @@
+$('.carousel').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
