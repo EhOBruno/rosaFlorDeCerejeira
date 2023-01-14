fbcda933bb7015a3e0815435ff2972ce5dfdc341
Júlia Fortunato <juliarochafort@gmail.com>
2023-01-14 15:40:17 -0300

    footer

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ function irParaCadastro() {
 function irParaLogin() {
   window.location.assign("./login.html")
 }
+
+
+let mybutton = document.getElementById("button-page-up");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+}
