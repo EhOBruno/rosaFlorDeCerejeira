9a288fa62111bc893a47392803eb37bf0e01cf21
Felipe de Oliveira Motta <felipemotta949@gmail.com>
2023-01-19 18:43:39 -0300

    header-com-login

---
+++ front/js/headerComlogin.js
@@ -0,0 +1,18 @@
+document.write(`
+<header class="header">
+  <div class="header-left">
+    <img src="../front/img/logo.svg" alt="logo" class="logo">
+    <input type="text" class="barra-pesquisa" 
+      placeholder="Pesquise aqui...">
+  </div>
+  <div class="header-right-all-login">
+    <a href="">
+      <span id="username">Username</span>
+    </a>
+    <a href="">
+      <img src="./img/login.svg" alt="" id="img-user">
+    </a>
+  </div>
+</header>
+`)
+
