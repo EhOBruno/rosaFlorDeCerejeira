847dbbf3e035148967dde7645ee3b9711fe7a614
Miguel <miguelmsoliveira@gmail.com>
2023-01-29 22:05:12 -0300

    meu saco

--- front/js/requisicao.js
+++ front/js/requisicao.js
@@ -1,3 +1,7 @@
+
+
+
+
 getJogos = async () => {
  await fetch("/filtros", {method:'POST'})
  .then(response => response.text())
@@ -7,5 +11,37 @@ getJogos = async () => {
 }); 
 }
 
-getJogos()
+// getJogos()
 
+teste = () => {
+
+    nome = document.getElementById('imputName').value
+    email = document.getElementById('imputEmail').value
+    password = document.getElementById('imputPassword').value
+    passwordConf = document.getElementById('imputPasswordConf').value
+    processador = document.getElementById('imputProcessador').value
+    gpu = document.getElementById('imputGpu').value
+    ram = document.getElementById('imputRam').value
+    data = {
+        "name": nome,
+        "email": email,
+        "pswd": password,
+        "passwordConf": passwordConf,
+        "gpu": gpu,
+        "ram": ram
+    }
+    req(data)
+    
+}
+
+req = async (data) => {
+    console.log(data)
+    alert()
+    await fetch("/cadastro", {method:'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)})
+    .then(response => response.json())
+    .then(jazonprovResponse => {
+       console.log(jazonprovResponse);
+       alert(jazonprovResponse)
+   }); 
+   alert('foi')
+}
